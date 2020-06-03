c747158b265c357e1b05f25fe22f55c8c35a1526
Tan Nguyen <tannguyen@blueberrylabs.com>
2020-06-03 13:32:51 +0700

    2 languages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Suspense } from 'react'
+import './i18n'
+import Hello from './components/Hello'
+import ThankYou from './components/Thankyou'
+import LanguageSelector from './components/LanguageSelector'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+      <div>
+        <Suspense fallback={null}>
+          <LanguageSelector />
+          <Hello />
+          <ThankYou />
+        </Suspense>
+      </div>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import * as serviceWorker from "./serviceWorker";
+
+i18next.init({
+  interpolation: { escapeValue: false }, // React already does escaping
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
